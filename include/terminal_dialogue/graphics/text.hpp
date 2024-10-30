dd13307325999bdace414981640f8ac43ac9e7d9
Juan Salazar <juansal1204@gmail.com>
2024-10-30 15:54:57 -0500

    Started Text and AnimatedText classes

---
+++ include/terminal_dialogue/graphics/text.hpp
@@ -0,0 +1,18 @@
+#ifndef TEXT_HPP
+#define TEXT_HPP
+
+class Text
+{
+public:
+  Text() {};
+  ~Text() {};
+};
+
+class AnimatedText : public Text
+{
+public:
+  AnimatedText() {};
+  ~AnimatedText() {};
+};
+
+#endif
