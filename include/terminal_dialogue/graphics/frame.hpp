5c668790212d2183020bf31edf32d883ff8c0fce
Juan Salazar <juansal1204@gmail.com>
2024-11-04 16:28:34 -0600

    Started Frame class to support graphical updates

---
+++ include/terminal_dialogue/graphics/frame.hpp
@@ -0,0 +1,15 @@
+#ifndef FRAME_HPP
+#define FRAME_HPP
+
+#include "terminal_dialogue/graphics/shape.hpp"
+#include "terminal_dialogue/graphics/rectangle.hpp"
+
+// TODO(juansala): Implement a Frame class that can contain a number of
+// graphical elements and draw them all at once (e.g. publish).
+class Frame
+{
+public:
+  Frame() {}
+};
+
+#endif
