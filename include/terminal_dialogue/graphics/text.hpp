78355a651a05c67b6f34529cba77f0fd4df2efbc
Juan Salazar <juansal1204@gmail.com>
2024-11-14 14:50:22 -0600

    Updated doc comments in Text class header file

--- include/terminal_dialogue/graphics/text.hpp
+++ include/terminal_dialogue/graphics/text.hpp
@@ -1,6 +1,12 @@
 #ifndef TEXT_HPP
 #define TEXT_HPP
 
+#include "terminal_dialogue/graphics/ncurses_wrapper.hpp"
+
+// This file declares text-relevant classes used for stylizing or animating
+// string-based messages such as menu options, dialogue, etc.
+// TODO(juansala): Implement animated dialogue.
+
 class Text
 {
 public:
